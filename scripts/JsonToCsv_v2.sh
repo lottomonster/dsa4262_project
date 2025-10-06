6b21b4fa7b21de8aea6186f78e55aaed22180853
L-Cherron <e0958318@u.nus.edu>
2025-10-06 22:31:32 +0800

    Added json to csv converter

---
+++ scripts/JsonToCsv_v2.sh
@@ -0,0 +1,10 @@
+zcat $1 |
+    awk -F "(:|])" '{gsub(/[{}"[]/, "", $0)
+        for (i=4;i<=NF;i++) {if (length($i) > 0){
+		n=split($i, a, ",");
+		printf "%s,%s,%s", $1, $2, $3;
+		{for (j=1; j<=n; j++){
+			if (length(a[j]) > 1){printf ",%s", a[j]};};
+		printf "\n"}}}}' >  $2
+
+sed -i '1i\ID,POS,SEQ,PreTime,PreSD,PreMean,InTime,InSD,InMean,PostTime,PostSD,PostMean,gene_id,label' $2
