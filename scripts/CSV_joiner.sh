ab8b44ed65f2d404ebcc5ade7432d2f06bdd3c3d
L-Cherron <e0958318@u.nus.edu>
2025-10-20 00:01:43 +0800

    More data processing including joins as per lao ban's request

---
+++ scripts/CSV_joiner.sh
@@ -0,0 +1,11 @@
+zcat $1 | head -n 1 |  awk -F',' '{print $1"_"$2","$0}' > header1.tmp
+zcat $2 | head -n 1 |  awk -F',' '{print "ID_POS,"$0}' > header2.tmp
+
+join -t ',' header1.tmp header2.tmp > combined.csv
+
+zcat $1 | awk -F',' 'NR>1 {print $1"_"$2","$0}' | sort -t ',' -k 1 > 1.tmp
+zcat $2 | awk -F',' 'NR>1 {print $1"_"$2","$0}' | sort -t ',' -k 1 > 2.tmp
+
+join -t ',' 1.tmp 2.tmp >> combined.csv
+
+cut -d, combined.csv -f2-13,17 | gzip -c > combined.csv.gz
